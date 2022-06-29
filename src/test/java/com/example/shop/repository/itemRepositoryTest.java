b4f6ea173613a8f294bb44a15cf9cfe70c904be7
ratellll <latellbin@naver.com>
2022-06-29 19:30:04 +0900

    Querydsl 조회 테스트

--- src/test/java/com/example/shop/repository/itemRepositoryTest.java
+++ src/test/java/com/example/shop/repository/itemRepositoryTest.java
@@ -1,13 +1,18 @@
 package com.example.shop.repository;
 
 import com.example.shop.Entity.Item;
+import com.example.shop.Entity.QItem;
 import com.example.shop.constant.ItemSellStatus;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -16,8 +21,11 @@ import java.util.List;
 @TestPropertySource(locations = "classpath:application-test.properties")
 class itemRepositoryTest {
 
+    @PersistenceContext
+    EntityManager em;
     @Autowired
     ItemRepository itemRepository;
+
 
     @Test
     @DisplayName("상품 저장 테스트")
@@ -106,6 +114,24 @@ class itemRepositoryTest {
         this.createItemList();
         List<Item> itemList = itemRepository.findByItemDetailBtNative("테스트 상품 상세 설명");
         for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트1")
+    public void queryDslTest(){
+        this.createItemList();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QItem qItem = QItem.item;
+        JPAQuery<Item> query  = queryFactory.selectFrom(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%" + "테스트 상품 상세 설명" + "%"))
+                .orderBy(qItem.price.desc());
+
+        List<Item> itemList = query.fetch();
+
+        for(Item item : itemList){
             System.out.println(item.toString());
         }
     }
