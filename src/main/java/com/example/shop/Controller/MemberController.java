426d2f63ac436e13d68df684e0c722b4ee48706c
ratellll <latellbin@naver.com>
2022-07-04 16:09:57 +0900

    멤버컨트롤러 수정

--- src/main/java/com/example/shop/Controller/MemberController.java
+++ src/main/java/com/example/shop/Controller/MemberController.java
@@ -5,15 +5,21 @@ import com.example.shop.dto.MemberFormDto;
 import com.example.shop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.shop.dto.MemberFormDto;
+import com.example.shop.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.validation.Valid;
+import com.example.shop.Entity.Member;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import org.springframework.validation.BindingResult;
+import javax.validation.Valid;
 
 @RequestMapping("/members")
 @Controller
@@ -23,20 +29,10 @@ public class MemberController {
     private final MemberService memberService;
     private final PasswordEncoder passwordEncoder;
 
-
     @GetMapping(value = "/new")
-    public String memberForm(Model model) {
+    public String memberForm(Model model){
         model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm";
-    }
-
-    @PostMapping(value = "/new")
-    public String memberForm(MemberFormDto memberFormDto) {
-
-        Member member = Member.createMember(memberFormDto, passwordEncoder);
-        memberService.saveMember(member);
-
-        return "redirect:/";
     }
 
     @PostMapping(value = "/new")
@@ -56,4 +52,16 @@ public class MemberController {
 
         return "redirect:/";
     }
+
+    @GetMapping(value = "/login")
+    public String loginMember(){
+        return "/member/memberLoginForm";
+    }
+
+    @GetMapping(value = "/login/error")
+    public String loginError(Model model){
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
+        return "/member/memberLoginForm";
+    }
+
 }
