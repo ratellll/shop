1db98c690b3c716a78cf7a558a738afb6347a971
ratellll <latellbin@naver.com>
2022-07-05 18:20:35 +0900

    고아 객체 삭제

---
+++ src/main/java/com/example/shop/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.example.shop.repository;
+
+
+import com.example.shop.Entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    
+}
