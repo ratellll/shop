edf93e10635d81cf0c54e9c1bf3f1dfc6e30183a
ratellll <latellbin@naver.com>
2022-07-05 20:26:26 +0900

    item img DTO및 엔티티 작성

---
+++ src/main/java/com/example/shop/Entity/ItemImg.java
@@ -0,0 +1,40 @@
+package com.example.shop.Entity;
+
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item_img")
+@Getter
+@Setter
+public class ItemImg extends BaseEntity {
+
+
+    @Id
+    @Column(name = "item_img_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String imgName;
+
+    private String oriImgName; // 원본 이미지 파일명
+
+    private String imgUrl; // 이미지 조회경로
+
+    private String repimgYn; //대표이미지 여부
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+
+    public void updateItemImg(String oriImgName, String imgName, String imgUrl) {
+        this.oriImgName = oriImgName;
+        this.imgName = imgName;
+        this.imgUrl = imgUrl;
+    }
+}
