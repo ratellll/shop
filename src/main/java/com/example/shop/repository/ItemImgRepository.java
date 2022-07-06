7830f8c9f3ef6f13298114afb66710e4e26ad03f
ratellll <latellbin@naver.com>
2022-07-06 20:22:00 +0900

    이미지 서비스  리포지토리추가

---
+++ src/main/java/com/example/shop/repository/ItemImgRepository.java
@@ -0,0 +1,9 @@
+package com.example.shop.repository;
+
+
+import com.example.shop.Entity.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+
+}
